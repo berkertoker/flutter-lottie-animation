3d46151db481ed02b158fb2e1566e27624cf4855
berkertoker <berkertoker2001@gmail.com>
2024-06-10 15:23:45 +0300

    update: main screen issue fixed

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:lottie_animation/home_page.dart';
 
 import 'package:flutter/material.dart';
-import 'package:lottie_animation/main_page.dart';
 
 void main() {
   runApp(MyAppp());
